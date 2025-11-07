ac25135107161ea646eaaec75b3fbf8ddec455f8
Kokora <henockkokora18@gmail.com>
2025-11-07 15:36:43 +0000

    commit

--- app/clients/annonce/[id]/page.js
+++ app/clients/annonce/[id]/page.js
@@ -893,7 +893,7 @@ export default function AnnonceDetails() {
                     <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z" />
                     </svg>
-                    Visiter la maison
+                    Visiter
                   </button>
                 </div>
                 <p className="mt-4 text-center text-xs text-gray-500">Aucun montant ne vous sera débité pour l'instant</p>
